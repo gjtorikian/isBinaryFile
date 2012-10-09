64981771d5d2aa49c3878c478b5446e079cf0db0
Garen Torikian <gjtorikian@gmail.com>
2012-10-08 22:39:20 -0700

    Implement.

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
 isBinaryFile
 ============
 
-Detects if a file is binary in Node.js. Similar to Perl's -B
+Detects if a file is binary in Node.js. Similar to [Perl's `-B` switch](http://stackoverflow.com/questions/899206/how-does-perl-know-a-file-is-binary), in that:
+
+* it reads the first few bytes of a file
+* checks for a `null` byte; if it's found, it's binary
+* flags non-ASCII characters. After a certain number of "weird" characters, the file is flagged as binary
+
+Please make sure the file exists before calling this function.
+
+## Installation
+
+```
+npm install isBinaryFile
+```
+
+## Usage
+
+```javascript
+var isBinaryFile = require("isBinaryFile");
+
+if (isBinaryFile(process.argv[2]))
+	console.log("It is!")
+else
+	console.log("No.")
+```
+
+Ta da.
---
+++ index.js
@@ -0,0 +1,26 @@
+var fs = require('fs');
+
+module.exports = function(file) {
+	// Read the file with no encoding for raw buffer access.
+	var bytes = fs.readFileSync(file); 
+	var size = fs.statSync(file).size;
+	var suspicious_bytes = 0;
+	var total_bytes = size > 1024 ? 1024 : size;
+	
+	for (var i = 0; i < total_bytes; i++) {
+    	if (bytes[i] == '\0') {
+    		// NULL char. It's binary
+            return 1;
+        }
+        else if ((bytes[i] < 7 || bytes[i] > 14) && (bytes[i] < 32 || bytes[i] > 127)) {
+            suspicious_bytes++;
+
+            // Disk IO is so slow that it's worthwhile to do this calculation after every suspicious byte.
+            // This is true even on a 1.6Ghz Atom with an Intel 320 SSD.
+            if ((suspicious_bytes * 100) / total_bytes > 10) {
+                return 1;
+            }
+        }
+	}
+	return 0;
+}
