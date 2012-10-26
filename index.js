43f1e06f6ba9979b6fa7179e071334d1c4426e7b
Garen Torikian <gjtorikian@gmail.com>
2012-10-25 22:12:02 -0700

    Make it more forgiving

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 var fs = require('fs');
 
-module.exports = function(bytes, statSize) {
-    var size = statSize || null;
-
+module.exports = function(bytes, size) {
     // Read the file with no encoding for raw buffer access.
     if (size === undefined) {
-        process.exit(1)
         bytes = fs.readFileSync(file);
         size = fs.statSync(file).size;
     } 
@@ -21,12 +18,16 @@ module.exports = function(bytes, statSize) {
         else if ((bytes[i] < 7 || bytes[i] > 14) && (bytes[i] < 32 || bytes[i] > 127)) {
             suspicious_bytes++;
 
-            // Disk IO is so slow that it's worthwhile to do this calculation after every suspicious byte.
-            // This is true even on a 1.6Ghz Atom with an Intel 320 SSD.
-            if ((suspicious_bytes * 100) / total_bytes > 10) {
+            // Read at least 32 bytes before making a decision
+            if (i > 32 && (suspicious_bytes * 100) / total_bytes > 20) {
                 return true;
             }
         }
     }
+
+    if ((suspicious_bytes * 100) / total_bytes > 20) {
+        return true;
+    }
+    
     return false;
 }
