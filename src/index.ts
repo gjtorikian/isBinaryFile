db72c42d5e98360bb51c28642d6cb5559cddc82f
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2019-12-29 11:39:22 +0000

    Release 4.0.3

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import * as fs from 'fs';
-import {promisify} from 'util';
+import { promisify } from 'util';
 
 const statAsync = promisify(fs.stat);
 const openAsync = promisify(fs.open);
 const closeAsync = promisify(fs.close);
 
-const MAX_BYTES = 512
+const MAX_BYTES = 512;
 
 export async function isBinaryFile(file: string | Buffer, size?: number): Promise<boolean> {
   if (isString(file)) {
@@ -22,13 +22,17 @@ export async function isBinaryFile(file: string | Buffer, size?: number): Promis
     return new Promise((fulfill, reject) => {
       fs.read(fileDescriptor, allocBuffer, 0, MAX_BYTES, 0, (err, bytesRead, _) => {
         closeAsync(fileDescriptor);
-        if (err) { reject(err) }
-        else { fulfill(isBinaryCheck(allocBuffer, bytesRead)); }
+        if (err) {
+          reject(err);
+        } else {
+          fulfill(isBinaryCheck(allocBuffer, bytesRead));
+        }
       });
     });
-  }
-  else {
-    if (size === undefined) { size = file.length; }
+  } else {
+    if (size === undefined) {
+      size = file.length;
+    }
     return isBinaryCheck(file, size);
   }
 }
@@ -47,16 +51,19 @@ export function isBinaryFileSync(file: string | Buffer, size?: number): boolean 
     fs.closeSync(fileDescriptor);
 
     return isBinaryCheck(allocBuffer, bytesRead);
-  }
-  else {
-    if (size === undefined) { size = file.length; }
+  } else {
+    if (size === undefined) {
+      size = file.length;
+    }
     return isBinaryCheck(file, size);
   }
 }
 
 function isBinaryCheck(fileBuffer: Buffer, bytesRead: number): boolean {
   // empty file. no clue what it is.
-  if (bytesRead === 0) { return false; }
+  if (bytesRead === 0) {
+    return false;
+  }
 
   let suspiciousBytes = 0;
   const totalBytes = Math.min(bytesRead, MAX_BYTES);
@@ -67,17 +74,35 @@ function isBinaryCheck(fileBuffer: Buffer, bytesRead: number): boolean {
   }
 
   // UTF-32 BOM
-  if (bytesRead >= 4 && fileBuffer[0] === 0x00 && fileBuffer[1] === 0x00 && fileBuffer[2] === 0xfe && fileBuffer[3] === 0xff) {
+  if (
+    bytesRead >= 4 &&
+    fileBuffer[0] === 0x00 &&
+    fileBuffer[1] === 0x00 &&
+    fileBuffer[2] === 0xfe &&
+    fileBuffer[3] === 0xff
+  ) {
     return false;
   }
 
   // UTF-32 LE BOM
-  if (bytesRead >= 4 && fileBuffer[0] === 0xff && fileBuffer[1] === 0xfe && fileBuffer[2] === 0x00 && fileBuffer[3] === 0x00) {
+  if (
+    bytesRead >= 4 &&
+    fileBuffer[0] === 0xff &&
+    fileBuffer[1] === 0xfe &&
+    fileBuffer[2] === 0x00 &&
+    fileBuffer[3] === 0x00
+  ) {
     return false;
   }
 
   // GB BOM
-  if (bytesRead >= 4 && fileBuffer[0] === 0x84 && fileBuffer[1] === 0x31 && fileBuffer[2] === 0x95 && fileBuffer[3] === 0x33) {
+  if (
+    bytesRead >= 4 &&
+    fileBuffer[0] === 0x84 &&
+    fileBuffer[1] === 0x31 &&
+    fileBuffer[2] === 0x95 &&
+    fileBuffer[3] === 0x33
+  ) {
     return false;
   }
 
@@ -100,16 +125,14 @@ function isBinaryCheck(fileBuffer: Buffer, bytesRead: number): boolean {
     if (fileBuffer[i] === 0) {
       // NULL byte--it's binary!
       return true;
-    }
-    else if ((fileBuffer[i] < 7 || fileBuffer[i] > 14) && (fileBuffer[i] < 32 || fileBuffer[i] > 127)) {
+    } else if ((fileBuffer[i] < 7 || fileBuffer[i] > 14) && (fileBuffer[i] < 32 || fileBuffer[i] > 127)) {
       // UTF-8 detection
       if (fileBuffer[i] > 193 && fileBuffer[i] < 224 && i + 1 < totalBytes) {
         i++;
         if (fileBuffer[i] > 127 && fileBuffer[i] < 192) {
           continue;
         }
-      }
-      else if (fileBuffer[i] > 223 && fileBuffer[i] < 240 && i + 2 < totalBytes) {
+      } else if (fileBuffer[i] > 223 && fileBuffer[i] < 240 && i + 2 < totalBytes) {
         i++;
         if (fileBuffer[i] > 127 && fileBuffer[i] < 192 && fileBuffer[i + 1] > 127 && fileBuffer[i + 1] < 192) {
           i++;
@@ -133,9 +156,11 @@ function isBinaryCheck(fileBuffer: Buffer, bytesRead: number): boolean {
 }
 
 function isString(x: any): x is string {
-  return typeof x === "string";
+  return typeof x === 'string';
 }
 
 function isStatFile(stat: fs.Stats): void {
-  if (!stat.isFile()) { throw new Error(`Path provided was not a file!`); }
+  if (!stat.isFile()) {
+    throw new Error(`Path provided was not a file!`);
+  }
 }
